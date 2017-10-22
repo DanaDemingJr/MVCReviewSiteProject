e5ca0ca0db3e9411379b86f373acd5111a58dab6
ddemingjr22 <ddemingjr22@yahoo.com>
2017-10-21 23:38:13 -0400

    more coding with controllers

--- MVCReviewSiteProject/Models/Reviews.cs
+++ MVCReviewSiteProject/Models/Reviews.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,18 @@ namespace MVCReviewSiteProject.Models
 {
     public class Reviews
     {
+        public int ID { get; set; }
+        public string ReviewerName { get; set; }
+        public string GadgetReviewed { get; set; }
+        public int DateReviewed { get; set; }
+        public string Review { get; set; }
+        [Required(ErrorMessage = "A rating is required")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
+        public int StarRating { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
 
     }
 }
